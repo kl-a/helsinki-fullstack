efec66a8dd8edd0c09903a7e09dc3020fb972bd7
kellyAleno <kellytsang@Kellys-MacBook-Air.local>
2019-06-30 21:25:57 +1000

    part1b done

--- part1/course_information/src/index.js
+++ part1/course_information/src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Header = props => {
+  console.log(props);
   return <h1>{props.name}</h1>;
 };
 
 const Part = props => {
+  console.log(props);
   return (
     <p>
-      {props.class[0]} {props.class[1]}
+      {props.class["name"]} {props.class["exercises"]}
     </p>
   );
 };
@@ -16,9 +18,9 @@ const Part = props => {
 const Content = props => {
   return (
     <div>
-      <Part class={props.classes[0]} />
-      <Part class={props.classes[1]} />
-      <Part class={props.classes[2]} />
+      <Part class={props.course.parts[0]} />
+      <Part class={props.course.parts[1]} />
+      <Part class={props.course.parts[2]} />
     </div>
   );
 };
@@ -28,17 +30,34 @@ const Total = props => {
 };
 
 const App = () => {
-  const course = "Half stack application development";
-  const classes = [
-    ["Fundamentals of react", 10],
-    ["Using props to pass data", 7],
-    ["State of a component", 14]
-  ];
+  const course = {
+    name: "Half stack application development",
+    parts: [
+      {
+        name: "Fundamentals of react",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  };
   return (
     <div>
-      <Header name={course} />
-      <Content classes={classes} />
-      <Total total={classes[0][1] + classes[1][1] + classes[2][1]} />
+      <Header name={course.name} />
+      <Content course={course} />
+      <Total
+        total={
+          course["parts"][0]["exercises"] +
+          course["parts"][1]["exercises"] +
+          course["parts"][2]["exercises"]
+        }
+      />
     </div>
   );
 };
